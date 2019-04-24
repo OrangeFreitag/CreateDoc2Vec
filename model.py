74c6be20eaab6df5a939622acd8dfe490a5260cd
Dominik Juelg <dominik.juelg@united-internet.de>
2019-04-24 10:37:34 +0200

    d4ed

--- model.py
+++ model.py
@@ -107,10 +107,10 @@ dropout = arguments.pop('dropout')
 num_epochs = arguments.pop('num_epochs')
 
 # 1. Load Data
-train_x = np.loadtxt("/data/shared-task/feat100_train_x.csv" ,delimiter='\t', usecols=range(11)[1:], skiprows=1)
-train_y = np.loadtxt("/data/shared-task/feat100_train_y.csv", delimiter='\t', usecols=range(3)[1:], skiprows=1)
-dev_test_x = np.loadtxt("/data/shared-task/feat100_test_x.csv", delimiter='\t', usecols=range(11)[1:], skiprows=1)
-dev_test_y = np.loadtxt("/data/shared-task/feat100_test_y.csv", delimiter='\t', usecols=range(3)[1:], skiprows=1)
+train_x = np.loadtxt("/data/shared-task/feat100fix_train_x.csv" ,delimiter='\t', usecols=range(11)[1:], skiprows=1)
+train_y = np.loadtxt("/data/shared-task/feat100fix_train_y.csv", delimiter='\t', usecols=range(3)[1:], skiprows=1)
+dev_test_x = np.loadtxt("/data/shared-task/feat100fix_test_x.csv", delimiter='\t', usecols=range(11)[1:], skiprows=1)
+dev_test_y = np.loadtxt("/data/shared-task/feat100fix_test_y.csv", delimiter='\t', usecols=range(3)[1:], skiprows=1)
 
 experiment.log_data_ref(data=train_x, data_name='train_x')
 experiment.log_data_ref(data=train_y, data_name='train_y')
